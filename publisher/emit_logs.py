1b3bcb3247587e71684d614781bbd5e880f8f979
fry <fercaffaro@gmail.com>
2016-05-27 19:02:36 -0300

    Comandos Spider v0.1

--- publisher/emit_logs.py
+++ publisher/emit_logs.py
@@ -1,23 +1,49 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pika
 import sys
 import time
+import getch
 
-credentials = pika.PlainCredentials('fer', 'caffaro')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.10.134',5672,'/',credentials))
+credentials = pika.PlainCredentials('user', 'user')
+connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1',5672,'/',credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='logs',
                          type='fanout')
 
-#cant_mensajes = join(sys.argv[1]) or 1
-#duracion_mensaje = join(sys.argv[3]) or 1
-#print(" cant: %s", cant_mensajes )
-#print(" dur: %s", cant_mensajes )
+print("---------------------------------------------------")
+print("| Mover la araña con las teclas         __        |")
+print("|                  W: Avanza         | /  \ |     |")
+print("|    W             A: Izquierda     \\_\\\\  //_/    |")
+print("|  A S D           S: Retrocede      -'/()\\'-     |")
+print("|                  D: Derecha         \\\  //      |")
+print("|    K             K: Kill them all               |")
+print("---------------------------------------------------")
 
-message = ' '.join(sys.argv[1:]) or "info: Hello World!"
 while 1:
-    channel.basic_publish(exchange='logs',routing_key='',body=message)
-    print(" [x] Sent %r" % message)
-    time.sleep(1)
+    char = getch.getch()
+    message  = str(char)
+    
+    if message == 'w' or message == 'W':
+       channel.basic_publish(exchange='logs',routing_key='',body=message)
+       print("Avanza")
+    if message == 'a' or message == 'A':
+       channel.basic_publish(exchange='logs',routing_key='',body=message)
+       print("Mover a la izquierda")
+    if message == 's' or message == 'S':
+       channel.basic_publish(exchange='logs',routing_key='',body=message)
+       print("Retroceder")
+    if message == 'd' or message == 'D':
+       channel.basic_publish(exchange='logs',routing_key='',body=message)
+       print("Mover a la derecha")
+    if message == 'k' or message == 'K':
+       channel.basic_publish(exchange='logs',routing_key='',body=message)
+       print("ta ta ta ta ta ta")
 connection.close()
+
+
+
+
+
+	
